c59a81ec1b8f8b98680021c1b698ae162b0a7d6b
Gabor Darvas <nagyfej@gmail.com>
2013-06-05 14:07:32 +0200

    admin template changed

---
+++ protected/views/layouts/admin.php
@@ -0,0 +1,129 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head>
+	<title><?php echo CHtml::encode($this->pageTitle); ?> Adminisztrációs felület</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+	<link rel="shortcut icon" href="admin/images/logo.png" />
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/admin_main.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/admin_login2.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/admin_form.css" media="screen, projection" />
+
+<?php /*
+<script type="text/javascript">
+{literal}
+$(document).ready(function(){
+
+	function keep_alive(){
+			$.ajax({
+				  url: 'admin.php',
+				  data: 'mod=keep_alive&ajax=true',
+				  type: "POST",
+				  context: document.body,
+				  success: function(data){
+					if(data != '1'){
+						alert('A kapcsolat megszakadt! További munka folytatása előtt kérem jelentkezzen be újra! Ha nem akarja, hogy a még nem mentett adatok elvesszenek, jelentkezzen be új böngészőablakban, majd mentse a munkát.');
+					}
+				  }
+			});
+	}
+	intervalID=setInterval(keep_alive,300000);
+
+	$("#message").hide().fadeIn();
+	$('.message_sima, .message_siker, .message_hiba').click(function(){ $(this).fadeOut(); });
+	setTimeout(function(){ $("#message").fadeOut(); }, 8000);
+
+	$('table#lista tr').live('click', function(){
+		var url=$(this).find('a').attr('href');
+		window.location.href = url;
+	});
+	$('table#lista tr td.ex').live('click', function(event){
+		event.stopPropagation();
+	});
+
+{/literal}
+{if $config.animacio == "on"}
+{literal}
+
+	$('#header ul li a').live('click', function(event){
+		event.preventDefault();
+		
+		$('#main_content').slideUp(200);
+		$('#footer').delay(200).slideUp(200);
+		$('div:last').after('<div class="container" id="ajax_loader"><img src="admin/images/ajax_loader.gif" /></div>');
+		
+		var current_url = $(this).attr('href');
+		var url_array=current_url.split('?');
+
+		$('#ajax_loader').ready(function(){
+			
+			$.ajax({
+				  url: 'admin.php',
+				  data: url_array[1]+'&ajax=true',
+				  type: "POST",
+				  context: document.body,
+				  success: function(data){
+					$('#main_content').delay(400).html(data).slideDown(400);
+					$('#footer').delay(800).slideDown(200);
+					$('#ajax_loader').delay(1000).fadeOut(300).delay(1300).remove();
+					window.location.href=current_url;
+				  }
+			});
+
+		});
+
+	});
+
+{/literal}
+{/if}
+{literal}
+	
+});
+{/literal}
+</script>
+ * */ ?>
+
+</head>
+<body>
+{include file="keret_message.php"}
+	<div class="container" id="header">
+		<div id="logo"><img src="" alt="<?php echo CHtml::encode(Yii::app()->name); ?>" /></div>
+		<div id="menu">
+			<ul>
+				<li><a href="{$smarty.server.PHP_SELF}?mod=kezdolap"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/admin/_menu_home.png" /> Kezdőlap</a></li>
+				<li><a href="{$smarty.server.PHP_SELF}?mod=statikus"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/admin/_menu_content.png" /> Modulok <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/admin/_menu_dropdown_arrow.png" /> </a>
+					<ul class="dropdown">
+						<li><a href="{$smarty.server.PHP_SELF}?mod=statikus">Tartalmi oldalak</a></li>
+						<li><a href="{$smarty.server.PHP_SELF}?mod=ajanlatkero">Ajánlatkérések</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<span id="right_menu">
+			<span id="logged_name">Bejelentkezve: <?php echo Yii::app()->user->name; ?></span><br class="float_right" />
+			<span id="settings">
+				<a class="settings" href="{$smarty.server.PHP_SELF}?mod=config">Beállítások </a>
+				<a class="logout" href="{$smarty.server.PHP_SELF}?mod=logout" title="Kilépés"></a>
+					<ul class="settings_dropdown">
+						<li><a href="{$smarty.server.PHP_SELF}?mod=jelszocsere"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/admin/settings_pswd_chng.png" />Jelszóváltoztatás</a></li>
+						<li><a href="{$smarty.server.PHP_SELF}?mod=admin"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/admin/settings_users.png" />Adminisztrátorok kezelése</a></li>
+						<li><a href="{$smarty.server.PHP_SELF}?mod=export_database"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/admin/settings_db_backup.png" />Adatbázis mentés</a></li>
+						<li><a href="{$smarty.server.PHP_SELF}?mod=config"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/admin/settings_site.png" />Oldal beállításai</a></li>
+						<li><a href="{$smarty.server.PHP_SELF}?mod=help"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/admin/settings_help.png" />Információ, segítségkérés</a></li>
+					</ul>
+			</span>
+		</span>
+	</div>
+	<div class="container" id="header_helper">
+	</div>
+	<div class="container" id="main_content">
+		<?php echo $content; ?>
+	</div>
+	<div class="container" id="footer">
+		<span style="float: left;">DD Standard Adminisztrációs Felület v3.0</span>
+		<span style="float: right"><a href="http://www.ddstandard.hu" target="_blank">www.ddstandard.hu</a></span>
+	</div>
+</body>
+</html>
