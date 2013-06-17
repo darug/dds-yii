81f4aa65deda221a1445970673345562c8bf20fb
Gabor Darvas <nagyfej@gmail.com>
2013-06-17 14:57:55 +0200

    test env files

---
+++ protected/config/db_prod.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+'class'=>'CDbConnection',
+'connectionString' => 'mysql:host=localhost;dbname=dds-yii',
+'emulatePrepare' => true,
+'username' => 'dds-yii',
+'password' => 'dds_yii',
+'charset' => 'utf8',
+);
