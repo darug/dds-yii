535eb3da2ab412dc46a96a9094b798e08687f0ad
Gabor Darvas <nagyfej@gmail.com>
2013-07-17 15:06:33 +0200

    remove logs and configs from repo

--- protected/config/db_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-'class'=>'CDbConnection',
-'connectionString' => 'mysql:host=localhost;dbname=dds-yii-test',
-'emulatePrepare' => true,
-'username' => 'dds-yii',
-'password' => 'dds_yii',
-'charset' => 'utf8',
-);
