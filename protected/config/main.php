38665766d7c34584c54562b512adab5d72fc281e
darug <dg@ddstandard.hu>
2013-08-21 14:19:19 +0200

    aprobb javitasok

--- protected/config/main.php
+++ protected/config/main.php
@@ -66,7 +66,7 @@ return array(
 					'ipFilters'=>array('127.0.0.1'),
 				),*/
 				// uncomment the following to show log messages on web pages
-				
+				/*
 				array(
 					'class'=>'CWebLogRoute',
 				),
